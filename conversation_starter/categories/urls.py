aaf6754a1ef99e359ab22117a6601148735ad83a
takdeerkaur <takdeerkaur@gmail.com>
2016-01-30 16:22:31 -0800

    Add endpoint to get category id from category name

--- conversation_starter/categories/urls.py
+++ conversation_starter/categories/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 	url(r'^$', views.api_root),
     url(r'^categories/$', views.CategoryList.as_view(), name='category-list'),
     url(r'^categories/(?P<pk>[0-9]+)/$', views.CategoryDetail.as_view(), name='category-detail'),
+    url(r'^categories/(?P<category>\w+)/$', views.CategoryIdByNameList.as_view(), name='category-name-by-id'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- conversation_starter/categories/views.py
+++ conversation_starter/categories/views.py
@@ -7,6 +7,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.response import Response
+from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @api_view(('GET',))
@@ -22,7 +24,21 @@ class CategoryList(generics.ListCreateAPIView):
     # def perform_create(self, serializer):
     # 	serializer.save(owner=self.request.user)
 
-
+class CategoryIdByNameList(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    def get_queryset(self):
+        """
+        This view should return a list of all the topics
+        under the given category.
+        """
+        category = self.kwargs['category']
+        # TODO: implement 404 if no category exists
+        # try:
+        #     Category.objects.filter(category_name=category)
+        # except ObjectDoesNotExist:
+        #     raise Http404("Category does not exist")
+        return Category.objects.filter(category_name=category)
 
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
